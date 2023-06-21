c84a02aca745a2d7c47d2d678333f8ea7c84fea5
yurim1578 <98522903+yurim1578@users.noreply.github.com>
2023-06-21 16:56:51 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/프로젝트 이름.jar"
+JAR_FILE="$PROJECT_ROOT/testMetasu.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
